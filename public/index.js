1027ac8adb3f3d74ac874dfa450c36e0125e3aa7
JWLD <jackwldavies@icloud.com>
2018-01-18 19:24:46 +0000

    add email form

--- public/index.js
+++ public/index.js
@@ -27,4 +27,39 @@
 			}, 500);
 		});
 	});
+
+	// toggle contact form
+	var formToggles = document.querySelectorAll('.form-toggle');
+
+	Array.from(formToggles).forEach((toggle) => {
+		toggle.addEventListener('click', function(e) {
+			e.preventDefault();
+
+			var formWrap = document.querySelector('#form-wrap');
+			formWrap.classList.toggle('show-form');
+		});
+	});
+
+	// send form
+	document.querySelector('#contact-form').addEventListener('submit', function(e) {
+		e.preventDefault();
+
+		var formValues = {};
+
+		var formElements = Array.from(e.target.elements);
+		formElements.forEach(input => input.name ? formValues[input.name] = input.value : null);
+
+		var data = JSON.stringify(formValues);
+
+		var xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				console.log('Success', xhr.responseText);
+			}
+		}
+		xhr.open('POST', '/contact');
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(formValues));
+	});
 })();
