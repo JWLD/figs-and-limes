eecf2f73efb0ed13f6faed2b644313592a3b3ce8
JWLD <jackwldavies@icloud.com>
2018-10-20 14:17:57 +0100

    reenable forced ssl

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const Handlebars = require('express-handlebars')
 const { bands, faqs } = require('./data.json')
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
-// const enforce = require('express-sslify')
+const enforce = require('express-sslify')
 require('dotenv').config()
 
 const app = Express()
 
-// app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 app.engine(
   'hbs',
